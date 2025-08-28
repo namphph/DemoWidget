f5387103c28357bbfb505978ce572d8e8e87d2d4
namphph <nampp@foxcode.org>
2025-08-28 09:45:12 +0700

    add widget use glance

---
+++ app/src/main/java/com/amazon/demowidget/widget/MyAppWidget.kt
@@ -0,0 +1,51 @@
+package com.amazon.demowidget.widget
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.Button
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.action.actionStartActivity
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.Text
+import com.amazon.demowidget.MainActivity
+
+class MyAppWidget: GlanceAppWidget() {
+
+    override suspend fun provideGlance(
+        context: Context,
+        id: GlanceId
+    ) {
+        provideContent {
+            myContent()
+        }
+    }
+
+    @Composable
+    private fun myContent() {
+        Column(
+            modifier = GlanceModifier.fillMaxSize(),
+            verticalAlignment = Alignment.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Where to?", modifier = GlanceModifier.padding(12.dp))
+            Row(horizontalAlignment = Alignment.CenterHorizontally) {
+                Button(
+                    text = "Home",
+                    onClick = actionStartActivity<MainActivity>()
+                )
+                Button(
+                    text = "Work",
+                    onClick = actionStartActivity<MainActivity>()
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/amazon/demowidget/widget/MyAppWidgetReceiver.kt
@@ -0,0 +1,8 @@
+package com.amazon.demowidget.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class MyAppWidgetReceiver() : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget =  MyAppWidget()
+}
